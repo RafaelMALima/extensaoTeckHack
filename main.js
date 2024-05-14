ca34872377d34eaaee8665c411547447ef1bc855
RafaelMALima <81188798+RafaelMALima@users.noreply.github.com>
2024-05-13 23:45:20 -0300

    remove debug border

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-document.body.style.border = "5px solid red";
+//uncomment for debug border 
+//document.body.style.border = "5px solid red";
 // Listen for network requests
 //
 browser.runtime.sendMessage({domain: window.location.hostname});
